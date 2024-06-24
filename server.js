9219ff20cffb65d14890b22c25250d65a2ce7ad8
anmol-kler <anmol.kler@student.kpu.ca>
2024-06-23 19:04:19 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+6
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+ 
+ console.log(`Server is running on port ${PORT}`);
+});
